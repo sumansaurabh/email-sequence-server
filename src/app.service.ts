4b69967914e9f4424f5128bbe0f2889a3db724e2
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-06-30 02:25:12 +0530

    refactor: Update User entity to use uppercase enum values for UserRole

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User, UserRole } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async onModuleInit() {
+    await this.createSuperuser();
+  }
+
+  private async createSuperuser() {
+    const superuser = await this.userRepository.findOne({ where: { email: 'sumanrocs@gmail.com' } });
+    if (!superuser) {
+      const password = await hash('alpha_beta_@31a', 10);
+      const newSuperuser = this.userRepository.create({
+        email: 'sumanrocs@gmail.com',
+        password,
+        role: UserRole.ADMIN,
+        isActive: true,
+      });
+      await this.userRepository.save(newSuperuser);
+      console.log('Superuser created');
+    }
+    else {
+      console.log('Superuser already exists');
+    }
+  }
+
   getHello(): string {
     console.log("Hello World app service!")
     return 'Hello World app service!';
